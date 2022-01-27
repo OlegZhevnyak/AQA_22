3412b700b1aa6aab1e0a2a6924674d7415a3e6df
OlegZhevnyak <ffieryice@gmail.com>
2022-01-28 02:29:54 +0300

    Refactor CalculatorRuner.class

--- Calculator/src/main/java/com/it_academy/calculator/CalculatorUtilities.java
+++ Calculator/src/main/java/com/it_academy/calculator/CalculatorUtilities.java
@@ -43,66 +43,68 @@ public class CalculatorUtilities {
 
     public static double userInput(String message) {
         Scanner input = new Scanner(System.in);
-        double d;
+        double doubleValue;
         while (true) {
             System.out.println(message);
             try {
-                d = Double.parseDouble(input.next());
+                doubleValue = Double.parseDouble(input.next());
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("You can only insert numbers and \".\". Please, repeat.");
             }
         }
-        return d;
+        return doubleValue;
     }
 
-    public static void doMathematicalOperation(){
-        
-        double result = 0;
-
+    public static void setValues() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Select a mathematical operation by typing one of the symbols: \"+ - * /\".");
+        setOperator(input.next().charAt(0));
+        setFirstValue(userInput("Insert first value:"));
+        setSecondValue(userInput("Insert second value:"));
+    }
 
+    public static void reportingOperationAndResult() {
+        System.out.println("Operation: " + getFirstValue() + " " + getOperator() + " " + getSecondValue());
+        System.out.println("Result: " + getResult());
+    }
+
+    public static void doAdditionOrSubtractionOrMultiplication() {
+        switch (getOperator()) {
+            case '+':
+                setResult(getFirstValue() + getSecondValue());
+                break;
+            case '-':
+                setResult(getFirstValue() - getSecondValue());
+                break;
+            case '*':
+                setResult(getFirstValue() * getSecondValue());
+                break;
+        }
+        reportingOperationAndResult();
+    }
+
+    public static void doDivision() {
+        if (getSecondValue() == 0) {
+            System.out.println("Division by zero is not available. Please, repeat.");
+        } else {
+        setResult(getFirstValue() / getSecondValue());
+        reportingOperationAndResult();
+        }
+    }
+
+    public static void doMathematicalOperation() {
         while (true) {
-            System.out.println("Select a mathematical operation by typing one of the symbols: \"+ - * /\".");
-            char operator = input.next().charAt(0);
-            CalculatorUtilities.setOperator(operator);
-
-            if ((operator == '+') || (operator == '-') || (operator == '*')) {
-                double firstValue = userInput("Insert first value.");
-                setFirstValue(firstValue);
-                double secondValue = userInput("Insert second value.");
-                setSecondValue(secondValue);
-
-                switch (operator) {
-                    case '+':
-                        result = firstValue + secondValue;
-                        break;
-                    case '-':
-                        result = firstValue - secondValue;
-                        break;
-                    case '*':
-                        result = firstValue * secondValue;
-                        break;
-                }
-                setResult(result);
-                System.out.println("Operation: " + firstValue + " " + operator + " " + secondValue);
-                System.out.println("Result: " + result);
+            if ((getOperator() == '+') || (getOperator() == '-') || (getOperator() == '*')) {
+                doAdditionOrSubtractionOrMultiplication();
                 break;
             }
-            if (operator == '/') {
-                double numerator = userInput("Insert numerator.");
-                double denominator = userInput("Insert denominator.");
-                if (denominator == 0) {
-                    System.out.println("Division by zero is not available. Please, repeat.");
-                    break;
-                }
-                result = numerator / denominator;
-                setResult(result);
-                System.out.println("Operation: " + numerator + " " + operator + " " + denominator);
-                System.out.println("Result: " + result);
+            if (getOperator() == '/') {
+                doDivision();
                 break;
             } else {
-                System.out.println("You insert invalid operation.");
+                System.out.println("You insert invalid operation. Please, repeat.");
+                break;
             }
         }
     }
